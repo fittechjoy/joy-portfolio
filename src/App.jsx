abd97db28c9c22cef6795cc23899eccf9ec109dc
fittechjoy <kiama01joy@gmail.com>
2025-12-17 09:14:09 +0300

    Initial commit: React + Tailwind portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Tailwind is working ✨
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
